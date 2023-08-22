90b1305aeafa5f0b898522a784a711b0c1a8112c
Guilherme <borimdev@gmail.com>
2023-08-22 11:22:08 -0300

    exercicio es6

--- main.js
+++ main.js
@@ -1,54 +1,14 @@
-class Veiculo {
-    constructor(marca, modelo) {
-        this.marca = marca;
-        this.modelo = modelo;
-    }
+const alunos = [
+    {nome: 'Guilherme', nota: '10'},
+    {nome: 'Fernanda', nota: '8'},
+    {nome: 'Simba', nota: '4'},
+    {nome: 'Iris', nota: '3'},
+]
 
-    ligar() {
-        throw new Error("Método 'ligar' deve ser implementado nas subclasses.");
-    }
-
-    desligar() {
-        throw new Error("Método 'desligar' deve ser implementado nas subclasses.");
-    }
-
+function filtrarAlunosAprovados(arrayDeAlunos) {
+    return arrayDeAlunos.filter(aluno => aluno.nota >= 6);
 }
+const alunosAprovados = filtrarAlunosAprovados(alunos);
 
-// Subclasse Carro
-class Carro extends Veiculo {
-    constructor(marca, modelo, numPortas) {
-        super(marca, modelo);
-        this.numPortas = numPortas;
-    }
-
-    ligar() {
-        return `O carro ${this.modelo} está ligado.`;
-    }
-
-    desligar() {
-        return `O carro ${this.modelo} está desligado.`;
-    }
-}
-
-// Subclasse Moto
-class Moto extends Veiculo {
-    constructor(marca, modelo, cilindradas) {
-        super(marca, modelo);
-        this.cilindradas = cilindradas;
-    }
-
-    ligar() {
-        return `A moto ${this.modelo} está ligada.`;
-    }
-
-    desligar() {
-        return `A moto ${this.modelo} está desligada.`;
-    }
-}
-
-// Criando instâncias
-const carro = new Carro("Toyota", "Corolla", 4);
-const moto = new Moto("Honda", "CBR500R", 500);
-const outroCarro = new Carro("Ford", "Mustang", 2);
-
-console.log(carro)
+const nomesAlunosAprovados = alunosAprovados.map(aluno => aluno.nome);
+console.log(`Alunos aprovados: ${nomesAlunosAprovados}`);
